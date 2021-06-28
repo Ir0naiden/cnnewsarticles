ba0bc1010f6d4c6f2218c98cbaa4359afbddb2bb
Aiden Baldwin <aidenbaldwin1985@gmail.com>
2021-06-28 22:42:13 +0100

    message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import './App.css';
-import React from 'react';
+import "./App.css";
+import React from "react";
 import Article from "./Article";
 
 import Chickens from "./img/chickenglasses.png";
-import Meat from "./img/meatvendor.png";
 import Pigs from "./img/pigsfly.png";
+import Meat from "./img/meatvendor.png";
 import Blanket from "./img/blanket.png";
-
-
 
 class App extends React.Component {
   render() {
     return (
       <div className="container">
-        
-        <h1 className="sectionTitle">Todays stories ...</h1>
-        <Article picture={Chickens} pictureAlt="Chickens with Glasses" newsHeadline="Chickens wear glasses" newsStory="Turns out that chickens do need to wear glasses to learn React." />
-        <Article picture={Pigs} pictureAlt="Pigs can fly" newsHeadline="Pigs do fly" newsStory="Pigs have started flying in north west England after a 4 week coding course." />
-        <Article picture={Meat} pictureAlt="Meat vendors" newsHeadline="Meat Vendors?" newsStory="Should we really buy sausages from vending machines?" />
-        <Article picture={Blanket} pictureAlt="Blanket" newsHeadline="Your new favourite band!" newsStory="4 lads from the northwest have created the album of the year" />
+        <h1 className="title">Todays top stories ...</h1>
+        <Article
+          picture={Chickens}
+          pictureAlt="Chickens with Glasses"
+          headline="Chickens wear glasses"
+          story="Turns out that chickens do need to wear glasses to learn React."
+        />
+        <Article
+          picture={Pigs}
+          pictureAlt="Pigs can fly"
+          headline="Pigs do fly"
+          story="Pigs have started flying in north west England after a 4 week coding course."
+        />
+        <Article
+          picture={Meat}
+          pictureAlt="Meat vendors"
+          headline="Meat Vendors?"
+          story="Should we really be buying sausages from vending machines?"
+        />
+        <Article
+          picture={Blanket}
+          pictureAlt="Blanket"
+          headline="Your new favourite band!"
+          story="Have these four lads from the northwest created the album of the year?"
+        />
       </div>
     );
   }
--- src/Article.js
+++ src/Article.js
@@ -1,11 +1,11 @@
 const Article = (props) => {
-    return (
-      <div className="article">
-        <img src={props.picture} alt={props.pictureAlt}/>
-        <h2>{props.newsHeadline}</h2>
-        <p>{props.newsStory}</p>
-      </div>
-    );
-  };
-  
-  export default Article;
+  return (
+    <div className="article">
+      <img src={props.picture} alt={props.pictureAlt} />
+      <h2>{props.headline}</h2>
+      <p>{props.story}</p>
+    </div>
+  );
+};
+
+export default Article;
